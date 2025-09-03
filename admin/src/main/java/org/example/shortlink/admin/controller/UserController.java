036b6ac9176c565fc604a2a8092275f7e95bcdb4
Jiannis <2459125166@qq.com>
2025-09-03 20:00:24 +0800

    更新根据用户名查询用户信息功能

--- admin/src/main/java/org/example/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/example/shortlink/admin/controller/UserController.java
@@ -3,7 +3,6 @@ package org.example.shortlink.admin.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.shortlink.admin.common.convention.result.Result;
 import org.example.shortlink.admin.common.convention.result.Results;
-import org.example.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.example.shortlink.admin.dto.resp.UserRespDTO;
 import org.example.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,11 +23,8 @@ public class UserController {
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
         UserRespDTO result = userService.getUserByUsername(username);
-        if(result == null) {
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-        } else {
-            return Results.success(result);
-        }
+        return Results.success(result);
+
 
     }
 }
