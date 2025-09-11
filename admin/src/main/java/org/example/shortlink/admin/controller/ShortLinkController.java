1cbaeaf305a09af0246642c1db67f899fd4ba6c8
Jiannis <2459125166@qq.com>
2025-09-11 16:30:32 +0800

    feature: 短链接项目后管系统调用中台请求相关接口

---
+++ admin/src/main/java/org/example/shortlink/admin/controller/ShortLinkController.java
@@ -0,0 +1,47 @@
+package org.example.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.example.shortlink.admin.common.convention.result.Result;
+import org.example.shortlink.admin.remote.dto.ShortLinkRemoteService;
+import org.example.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import org.example.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import org.example.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import org.example.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 短链接后管控制层
+ */
+@RestController
+public class ShortLinkController {
+
+    /**
+     * 后续重做为SpringCloud Feign调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 创建短链接
+     * @param requestParam 创建短链接请求参数
+     * @return 创建短链接返回参数
+     */
+    @PostMapping("/api/short-link/admin/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam){
+        return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 分页查询短链接
+     * @param requestParam 分页查询短链接请求参数
+     * @return 分页查询短链接返回参数
+     */
+    @GetMapping("/api/short-link/admin/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam){
+        return shortLinkRemoteService.pageShortLink(requestParam);
+    }
+}
